eacaddb2951837e46d508d1a7f6be7bb987c2b52
Ethan <2730611568@qq.com>
2026-03-09 20:34:31 +0800

    项目本地化配置

--- sky-server/src/main/java/com/sky/service/EmployeeService.java
+++ sky-server/src/main/java/com/sky/service/EmployeeService.java
@@ -3,7 +3,7 @@ package com.sky.service;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 
-public interface EmployeeService {
+public interface  EmployeeService {
 
     /**
      * 员工登录
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -13,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.util.Arrays;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -28,7 +30,6 @@ public class EmployeeServiceImpl implements EmployeeService {
     public Employee login(EmployeeLoginDTO employeeLoginDTO) {
         String username = employeeLoginDTO.getUsername();
         String password = employeeLoginDTO.getPassword();
-
         //1、根据用户名查询数据库中的数据
         Employee employee = employeeMapper.getByUsername(username);
 
@@ -39,6 +40,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
+        // 对前端传过来的密码进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         // TODO 后期需要进行md5加密，然后再进行比对
         if (!password.equals(employee.getPassword())) {
             //密码错误
